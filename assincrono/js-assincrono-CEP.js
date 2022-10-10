c9f43b97da579000345b5a40557727c5039d4e81
Marcelo Damasceno <marcelodamasceno@gmail.com>
2022-10-09 22:42:34 -0300

    adiciona página html

--- assincrono/js-assincrono-CEP.js
+++ assincrono/js-assincrono-CEP.js
@@ -1,9 +1,13 @@
-const apiUrl='https://viacep.com.br/ws/01001000/json/'
-const enderecoHTML=document.querySelector('#endereco')
+const apiUrl = 'https://viacep.com.br/ws/01001000/json/'
+const enderecoHTML = document.querySelector('#endereco')
+const btnGetEndereco = document.querySelector("#getEndereco")
 
-fetch(apiUrl)
-  .then(dados=>dados.json())
-  .then(resposta=>{
-    enderecoHTML.innerHTML=`<p>${resposta.logradouro}</p>`
-  })
-.catch(resposta=>enderecoHTML.innerHTML="<p>Erro</p>")
+btnGetEndereco.addEventListener('click', (event) => {
+  event.preventDefault()
+  fetch(apiUrl)
+    .then(dados => dados.json())
+    .then(resposta => {
+      enderecoHTML.innerHTML = `<p>${resposta.logradouro}</p>`
+    })
+    .catch(resposta => enderecoHTML.innerHTML = "<p>Erro</p>")
+})
